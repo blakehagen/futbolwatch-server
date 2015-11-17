cf6a2ee92fe3345dda86daff7d7187f7cdc22ee1
Blake Hagen <blakehagen@gmail.com>
2015-11-17 03:00:25 -0700

    new api

--- controllers/mainCtrl.js
+++ controllers/mainCtrl.js
@@ -8,7 +8,8 @@ module.exports = {
 
         var options = {
             // uri: 'http://pokeapi.co/api/v1/pokemon/1/',
-            uri: 'http://www.xmlsoccer.com/FootballData.asmx/GetLeagueStandingsBySeason?ApiKey=ZDWHYYRPDCFXURDLPDTOFOAQCALBHWCPUYKXIKZJSHNVDKCKUK' + /*config.secretKey*/ + '&league=54&seasonDateString=1516',
+            uri: 'https://www.kimonolabs.com/api/atzhegky?apikey=Gw5PcHFe2RGyDfn2GJlUBynbruB41WWo',
+            // uri: 'http://www.xmlsoccer.com/FootballData.asmx/GetLeagueStandingsBySeason?' + config.secretKey + '&league=54&seasonDateString=1516',
             // qs: {
             //     access_token: 'xxxxx xxxxx' // -> uri + '?access_token=xxxxx%20xxxxx' 
             // },
@@ -28,7 +29,7 @@ module.exports = {
                     console.log(result);
                     dataObj = result;
                 });
-                res.status(200).json(dataObj);
+                res.status(200).json(data);
             })
             .catch(function (err) {
                 console.log('API call failed');   // API call failed... 
