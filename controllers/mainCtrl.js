df9c699e5ef98f0e336529a3aaf9d038b05625e2
Blake Hagen <blakehagen@gmail.com>
2015-11-17 03:05:03 -0700

    testing

--- controllers/mainCtrl.js
+++ controllers/mainCtrl.js
@@ -8,8 +8,8 @@ module.exports = {
 
         var options = {
             // uri: 'http://pokeapi.co/api/v1/pokemon/1/',
-            uri: 'https://www.kimonolabs.com/api/atzhegky?apikey=Gw5PcHFe2RGyDfn2GJlUBynbruB41WWo',
-            // uri: 'http://www.xmlsoccer.com/FootballData.asmx/GetLeagueStandingsBySeason?' + config.secretKey + '&league=54&seasonDateString=1516',
+            // uri: 'https://www.kimonolabs.com/api/atzhegky?apikey=Gw5PcHFe2RGyDfn2GJlUBynbruB41WWo',
+            uri: 'http://www.xmlsoccer.com/FootballData.asmx/GetLeagueStandingsBySeason?' + config.secretKey + '&league=54&seasonDateString=1516',
             // qs: {
             //     access_token: 'xxxxx xxxxx' // -> uri + '?access_token=xxxxx%20xxxxx' 
             // },
@@ -23,13 +23,14 @@ module.exports = {
             .then(function (data) {
                 var dataObj;
                 // Convert XML to JSON-like Object
-                var xml = data
+                var xml = data;
+                console.log(xml);
                 parseString(xml, function (err, result) {
-                    // console.dir(result);
+                    console.dir(result);
                     console.log(result);
                     dataObj = result;
                 });
-                res.status(200).json(data);
+                res.status(200).json(dataObj);
             })
             .catch(function (err) {
                 console.log('API call failed');   // API call failed... 
