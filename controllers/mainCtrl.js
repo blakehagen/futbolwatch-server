99594f0478161953c34d11dd6bcf5b81b2dc64f2
Blake Hagen <blakehagen@gmail.com>
2015-11-16 19:07:20 -0700

    working

--- controllers/mainCtrl.js
+++ controllers/mainCtrl.js
@@ -1,11 +1,13 @@
 var rp = require('request-promise');
+var parseString = require('xml2js').parseString;
+var config = require('../config');
 
 module.exports = {
 
     getData: function (req, res, next) {
 
         var options = {
-            uri: 'http://pokeapi.co/api/v1/type/3/',
+            uri: 'http://www.xmlsoccer.com/FootballData.asmx/GetLeagueStandingsBySeason?' + config.secretKey + '&league=10&seasonDateString=1516',
             // qs: {
             //     access_token: 'xxxxx xxxxx' // -> uri + '?access_token=xxxxx%20xxxxx' 
             // },
@@ -16,11 +18,19 @@ module.exports = {
         };
         rp(options)
             .then(function (data) {
-                console.log('this is the data', data);
-                res.status(200).json(data);
+                var dataObj;
+                // Convert XML to JSON-like Object
+                var xml = data
+                parseString(xml, function (err, result) {
+                    console.dir(result);
+                    console.log(result);
+                    dataObj = result;
+                });
+
+                res.status(200).json(dataObj);
             })
             .catch(function (err) {
-                // API call failed... 
+                console.log('API call failed');   // API call failed... 
             });
 
 
--- server.js
+++ server.js
@@ -1,21 +1,18 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var rp = require('request-promise');
-var MainCtrl = require('./controllers/mainCtrl')
+var cors = require('cors');
+var parseString = require('xml2js')
+
+var MainCtrl = require('./controllers/mainCtrl');
 
 var app = express();
 
 app.use(bodyParser.json());
 
+app.use(cors());
 
 app.get('/data', MainCtrl.getData);
-
-
-
-
-
-
-
 
 
 app.listen(3000, function () {
