2c73cf2438861efd05de46f895ccf50b0fa2dcf3
Blake Hagen <blakehagen@gmail.com>
2015-11-17 02:50:20 -0700

    changed url test

--- controllers/mainCtrl.js
+++ controllers/mainCtrl.js
@@ -7,8 +7,8 @@ module.exports = {
     getData: function (req, res, next) {
 
         var options = {
-            // uri: 'http://pokeapi.co/api/v1/pokemon/1/',
-            uri: 'http://www.xmlsoccer.com/FootballData.asmx/GetLeagueStandingsBySeason?' + config.secretKey + '&league=50&seasonDateString=1516',
+            uri: 'http://pokeapi.co/api/v1/pokemon/1/',
+            // uri: 'http://www.xmlsoccer.com/FootballData.asmx/GetLeagueStandingsBySeason?' + config.secretKey + '&league=50&seasonDateString=1516',
             // qs: {
             //     access_token: 'xxxxx xxxxx' // -> uri + '?access_token=xxxxx%20xxxxx' 
             // },
@@ -24,11 +24,11 @@ module.exports = {
                 // Convert XML to JSON-like Object
                 var xml = data
                 parseString(xml, function (err, result) {
-                    console.dir(result);
-                    console.log(result);
+                    // console.dir(result);
+                    // console.log(result);
                     dataObj = result;
                 });
-                res.status(200).json(dataObj);
+                res.status(200).json(data);
             })
             .catch(function (err) {
                 console.log('API call failed');   // API call failed... 
