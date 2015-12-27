fd2bd6236cced954ca2eea9912f865c276cf487c
Blake Hagen <blakehagen@gmail.com>
2015-12-27 09:57:32 -0700

    extended time range for previous matches

--- controllers/mainCtrl.js
+++ controllers/mainCtrl.js
@@ -5,7 +5,7 @@ module.exports = {
     getResults: function (req, res, next) {
         var league = req.query.league;
         var options = {
-            uri: 'http://api.football-data.org/alpha/soccerseasons/' + league + '/fixtures/?timeFrame=p120',
+            uri: 'http://api.football-data.org/alpha/soccerseasons/' + league + '/fixtures/?timeFrame=p180',
             headers: { 'X-Auth-Token': '57d24f023e8247aea4badd00e37328dc' },
             json: true // Automatically parses the JSON string in the response 
         };
