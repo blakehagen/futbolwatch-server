cde99fee8b56563db08bcf0484b94248d6a3e24f
Blake Hagen <blakehagen@gmail.com>
2017-09-19 19:29:48 -0600

    fix fixtures league ids

--- server/features/upcoming-fixtures/server.fixtures.controller.js
+++ server/features/upcoming-fixtures/server.fixtures.controller.js
@@ -7,6 +7,34 @@ module.exports = {
   getFixtures: (req, res) => {
     let league = req.query.league;
 
+    // UPDATE LEAGUE IDs for 2017/18 seasons (9.19.2017) //
+    switch(league) {
+      case '436': // la liga
+        league = '455';
+        break;
+      case '430': // bundesliga
+        league = '452';
+        break;
+      case '433':
+        league = '449'; // eredivisie
+        break;
+      case '426': // premier league
+        league = '445';
+        break;
+      case '438': // serie a
+        league = '456';
+        break;
+      case '439': // portugal primeira
+        league = '457';
+        break;
+      case '434': // ligue 1
+        league = '450';
+        break;
+      case '440': // champions league
+        league = '464';
+        break;
+    }
+
     let options = {
       uri: 'http://api.football-data.org/alpha/soccerseasons/' + league + '/fixtures/?timeFrame=n21',
       headers: {'X-Auth-Token': '57d24f023e8247aea4badd00e37328dc'},
